74b036aebe2c10a6e23432713433b32482d68342
niroshan <48372147+vniroshan@users.noreply.github.com>
2021-05-16 00:58:36 +0530

    Initial commit

---
+++ src/main/java/com/niroshan/SpringJDBC/SpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.niroshan.SpringJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApplication.class, args);
+    }
+
+}
